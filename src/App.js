ff1f804fb8566e085047c68952267c9335bdeefd
Yuliya Zhezhel <yuliya.zhezhel@gmail.com>
2020-04-03 10:13:55 +0300

    add pictures

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import logoSkype from './images/logo-skype.svg';
+import logoPhone from './images/logo-skype.svg';
+import logoMail from './images/logo-mail.svg';
+import logoGit from './images/logo-github.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="">
+                <div className="header-speciality">
+                    Frontend Developer
+                </div>
+                <div className="header-name">
+                    Yuliya Zhezhel
+                </div>
+
+            </header>
+            <div className="header_contact-info">
+                <div className="header_contact-info_skype">
+                    <img src={logoSkype} alt="logo-skype"/>
+                    <a href="skype:yuliya_zhezhel">yuliya_zhezhel</a>
+                </div>
+                <div className="header_contact-info_phone">
+                    <img src={logoPhone}  alt="logo-phone"/>
+                    <a href="tel:+375447401571">+375447401571</a>
+                </div>
+                <div className="header_contact-info_mail">
+                    <img src={logoMail} alt="logo-mail"/>
+                    <a href="mailto:yuliya.zhezhel@gmail.com">yuliya.zhezhel@gmail.com</a>
+                </div>
+                <div className="header_contact-info_git">
+                    <img src={logoGit} alt="logo-github"/>
+                    <a href="https://github.com/yuliya-zhezhel">Github</a>
+
+                </div>
+        </div>
+        </div>
+    );
 }
 
 export default App;
